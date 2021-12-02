bc543a6ad207dc1f728ba1c312eb99428daccbad
mentat51 <sylvain.fauveau@gmail.com>
2021-12-02 15:40:19 +0100

    added woodsprite

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ const habitants = {
 }
 
 const food = {
-    zone4: `Choux, navets, betteraves... Que des légumes tristes, car rien d'autre ne pousse dans leur région. Leur pain est très mauvais, évitez le. Il est fortement conseillé d'apporter sa propre nourriture. Et sa propre boisson, car il est déjà arrivé qu'ils empoisonnent l'eau pour une querelle de voisinage... Photo de testicules de poulets, met très prisé par les locaux.`,
+    zone4: `Choux, navets, betteraves... Que des légumes tristes, car rien d'autre ne pousse dans leur région. Leur pain est très mauvais, évitez le. Il est fortement conseillé d'apporter sa propre nourriture. Et sa propre boisson, car il est déjà arrivé qu'ils empoisonnent l'eau pour une querelle de voisinage... Photo de testicules de poulets, mets très prisé par les locaux.`,
 }
 
 const nature = {
@@ -69,6 +69,7 @@ function clickOnZone(event){
     const title = document.getElementById('name-area');
     title.innerHTML = titles[zone];
     title.scrollIntoView();
+    launchAnimation();
 }
 
 // On assigne les callback quand on clique sur la barre nav
@@ -79,4 +80,32 @@ for (const li of document.querySelectorAll('nav li')){
 // On assigne les callback quand on clique sur les marqueurs de la carte
 for (const a of document.querySelectorAll('header a')){
     a.onclick = clickOnZone;
+}
+
+/* WoodSprite animation */
+let idAnimation = null;
+function launchAnimation() {
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min +1)) + min;
+    }
+    const img = document.getElementById('woodSprite');
+    const vw = window.innerWidth;
+    const vh = window.innerHeight;
+
+    let posX = 0, posY = vw/2;
+    clearInterval(idAnimation);
+    idAnimation = setInterval(frame, 10);
+    function frame() {
+        if (posY > vh+img.height+50) {
+            clearInterval(idAnimation);
+        } else {
+            posX++;
+            posY += getRandomIntInclusive(-1,1);
+
+            img.style.top = posX + 'px';
+            img.style.left = posY + 'px';
+        }
+    }
 }
